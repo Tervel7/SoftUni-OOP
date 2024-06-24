d5b8ecf64d1f7357d37179a025a2523a1c071b84
Dany <danijeliazkov@yahoo.bg>
2024-06-24 20:01:56 +0300

    changing directories

--- Enums/JediGalaxy/JediGalaxy.java
+++ Enums/JediGalaxy/JediGalaxy.java
@@ -45,5 +45,6 @@ public class JediGalaxy {
         }
 
         System.out.println(score);
+        // print
     }
 }
--- src/Enums/TrafficLights/Color.java
+++
@@ -1,5 +0,0 @@
-package Enums.TrafficLights;
-
-public enum Color {
-    RED, YELLOW, GREEN
-}
