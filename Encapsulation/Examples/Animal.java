5f4e34aa087ddc4e78911e516ef20c77bcd3d65b
Dany <danijeliazkov@yahoo.bg>
2024-06-24 22:33:55 +0300

    OOP encapsulation exercises

---
+++ Encapsulation/Examples/Animal.java
@@ -0,0 +1,11 @@
+package Encapsulation.Examples;
+
+public class Animal {
+    protected int height;
+    private int weight;
+
+    Animal(int height, int weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+}
